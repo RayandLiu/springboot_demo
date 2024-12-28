5517d2fbc3ca063fb864f46c90ac171b9b1f0710
Ray <19979016465@163.com>
2024-12-28 16:56:20 +0800

    完成删除功能

--- src/main/java/org/example/controller/BookController.java
+++ src/main/java/org/example/controller/BookController.java
@@ -11,4 +11,9 @@ public class BookController {
         System.out.println("id==>"+id);
         return "List of one book";
     }
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable Integer id) {
+        System.out.println("id==>"+id);
+        return "Deleted book";
+    }
 }
